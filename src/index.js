84da13d0d16b72c1cc833d4377c91f6e045a0241
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-09-07 11:38:36 +0530

    added rules

---
+++ src/index.js
@@ -0,0 +1,44 @@
+export const rules = {
+    "no-top-level-use-client": {
+        create(context) {
+            return {
+                Program(node) {
+                    const filename = context.getFilename();
+                    if (filename.endsWith('src/app/page.tsx')) {
+                        node.body.forEach((statement) => {
+                            if (statement.type === "ExpressionStatement" &&
+                                statement.expression.type === "Literal" &&
+                                statement.expression.value === "use client") {
+                                context.report({
+                                    node: statement,
+                                    message: 'Top-level "use client" directive may cause problems to existing stack.You\'re importing a component that needs server-only. That only works in a Server Component which is not supported in the pages/ directory.',
+                                });
+                            }
+                        });
+                    }
+                },
+            };
+        },
+    },
+    "no-event-handlers-in-client-props": {
+        create(context) {
+            return {
+                JSXAttribute(node) {
+                    const filename = context.getFilename();
+                    if (filename.endsWith('src/app/page.tsx')) {
+                        if (node.value && node.value.type === 'JSXExpressionContainer') {
+                            const value = node.value.expression;
+                            if ((value.type === 'ArrowFunctionExpression' || value.type === 'FunctionExpression') &&
+                                node.name.name.startsWith('on')) {
+                                context.report({
+                                    node,
+                                    message: 'Event handlers cannot be passed to Client Component props.',
+                                });
+                            }
+                        }
+                    }
+                },
+            };
+        },
+    },
+};                                
